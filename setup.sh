1869d2f490e255f1cef071345256b6069b18ff46
xk3 <7908073+chapmanjacobd@users.noreply.github.com>
2021-10-17 13:05:23 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 
 set -Eeuox pipefail
 
+sudo systemctl status fstrim.timer
+sudo systemctl enable fstrim.timer
+sudo systemctl start fstrim.timer
+
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf groupupdate –with-optional Multimedia
+sudo dnf group update core
+sudo dnf --with-optional -y groupinstall Multimedia
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
 
 ssh-keygen -t ed25519 -f ~/.ssh/id
 
@@ -12,7 +19,9 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 
 dnf check-update
 
-sudo dnf install -y mpv ffmpeg python3 python3-pip git android-tools R ImageMagick bat breaktimer code copyq darktable java-latest-openjdk detox cowsay dnscrypt-proxy exa evince expect fd fdk-aac fdupes fish gdal fzf gdal-devel ffmpeg-devel gdal-java geos-devel gdal-python-tools gimp git-delta git-lfs htop inotify-tools jq kdiff3 keepassxc kitty krename lato-fonts lcdnurse libpq-devel libreoffice mediawriter meld moreutils ncdu nmon nomacs pdfmod pg_top picard pipenv plasma-workspace-x11 podman-docker pspg python3-qgis python3.8 ripgrep rstudio-desktop syncthing udunits2 wine xdotool xclip xbacklight trash-cli tldr wget fedora-workstation-repositories chromium-freeworld dnf-plugins-extras-tracer cpufrequtils postgresql-devel breeze-gtk gdal-devel sqlite-devel bash-completion fd-find
+sudo dnf install -y mpv ffmpeg python3 python3-pip git android-tools R ImageMagick bat code copyq darktable java-latest-openjdk detox cowsay dnscrypt-proxy exa evince expect fdk-aac fdupes fish gdal fzf gdal-devel ffmpeg-devel gdal-java geos-devel gdal-python-tools gimp git-delta git-lfs htop inotify-tools jq kdiff3 keepassxc kitty krename lato-fonts libpq-devel libreoffice mediawriter meld moreutils ncdu nmon nomacs pdfmod pg_top picard pipenv plasma-workspace-x11 podman-docker pspg python3-qgis python3.8 ripgrep rstudio-desktop syncthing udunits2 wine xdotool xclip xbacklight trash-cli tldr wget fedora-workstation-repositories chromium-freeworld dnf-plugins-extras-tracer cpufrequtils postgresql-devel breeze-gtk gdal-devel sqlite-devel bash-completion fd-find krusader xmodmap
+
+setxkbmap us -variant altgr-intl -option caps:backspace
 
 wget https://github.com/tom-james-watson/breaktimer-app/releases/latest/download/BreakTimer.AppImage -P ~/.local/bin/
 
@@ -37,10 +46,15 @@ sudo dnf install -y google-cloud-sdk
 
 sudo dnf install -y https://dbeaver.com/files/dbeaver-ee-latest-stable.x86_64.rpm
 
+sudo loginctl enable-linger xk
 
-sudo dnf erase kcalc PackageKit konversation falkon dragon konversation falkon kget ktorrent konqueror chromium zoom docker podman docker podman autokey kontact dolphin Kontact dolphin kwrite calligra* korganizer kmail akregator knode krdc krfb konqueror ktnef kaddressbook konversation kf5-akonadi-server mariadb-common
+sudo dnf erase kcalc PackageKit konversation falkon dragon konversation falkon kget ktorrent konqueror chromium docker autokey kontact dolphin Kontact dolphin kwrite calligra* korganizer kmail akregator knode krdc krfb konqueror ktnef kaddressbook konversation kf5-akonadi-server mariadb-common kmail kontact akregator dragon kmag kmahjongg kmines kmousetool korganizer kwrite kaddressbook elisa-player
 # sudo dnf erase plasma-discover
+
+chsh -s /bin/fish
 
 gcloud init
 gcloud auth application-default login
 gcloud auth login
+#https://www.ctrl.blog/entry/fedora-hibernate.html
+ln -s d/31_Downloads/ Downloads
